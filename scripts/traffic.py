e38307be409c1aded663e73119491f5fb06afa72
earthPerson-001 <85444310+earthPerson-001@users.noreply.github.com>
2023-07-17 12:02:34 +0545

    Making it possible to run both the simulations at once. Parallize using multiprocessing. Remove redundant setting of traffic light phase in the hopes of reducing the time taken for the simulation with RL.

--- scripts/traffic.py
+++ scripts/traffic.py
@@ -33,6 +33,8 @@ import numpy
 from generateRouteFile import generate_routefile
 from setupLaneCounting import setupLaneCounting
 
+from multiprocessing import Process
+
 # checking for sumo_home variable and exiting if it is not found
 if 'SUMO_HOME' in os.environ:
     tools = os.path.join(os.environ['SUMO_HOME'], 'tools')
@@ -67,7 +69,7 @@ ROOT = Path(os.path.relpath(ROOT, Path.cwd()))  # relative
 
 # constants
 TRAFFIC_INTERSECTION_TYPE="triple"
-TOTAL_TIMESTEPS=50000  # This is the sumo timestep which is somewhat independent of steps in simulation
+TOTAL_TIMESTEPS=500000  # This is the sumo timestep which is somewhat independent of steps in simulation
                        # rather this decides the number of steps in the simulation
 GENERATE_CUSTOM_ROUTE=False
 LOG_TO_FILE=True # generating log of lane vehicle stats
@@ -85,9 +87,10 @@ use_gui = True # gui
 # sumo stuffs
 SUMO_DIRECTORY= Path(str(ROOT) + "/sumo-files").resolve()
 net_file=Path(str(SUMO_DIRECTORY) + "/small-map-{}-lane.net.xml".format(TRAFFIC_INTERSECTION_TYPE)).resolve()
-route_file=Path(str(SUMO_DIRECTORY) + "/small-map-{}-lane.rou.xml".format(TRAFFIC_INTERSECTION_TYPE)).resolve()
+route_file=Path(str(SUMO_DIRECTORY) + "/test-small-map-{}-lane.rou.xml".format(TRAFFIC_INTERSECTION_TYPE)).resolve()
 sumocfg_file=Path(str(SUMO_DIRECTORY) + "/small-map-{}-lane.sumocfg".format(TRAFFIC_INTERSECTION_TYPE)).resolve()
-additional_file=Path(str(SUMO_DIRECTORY) + "/countlanes-triple-lanes.add.xml").resolve()
+additional_file_no_rl=Path(str(SUMO_DIRECTORY) + "/countlanes-triple-lanes.add.xml").resolve()
+additional_file_rl=Path(str(SUMO_DIRECTORY) + "/copyof.countlanes-triple-lanes.add.xml").resolve()
 logging_output_folder=Path(str(ROOT) + "/logging_outputs").resolve()
 
 
@@ -96,12 +99,19 @@ models_path = Path(str(ROOT) + "/models").resolve()
 
 # Here, formatting is done as to create error if wrong model is selected
 # as, there won't be same model trained at exact same time and upto same timesteps
-model_path = Path(str(models_path) + "/2022-11-22 23 03 49.442229-TrafficIntersection-{}LaneGUI-ppo-450000".format(TRAFFIC_INTERSECTION_TYPE.capitalize())).resolve()
+model_path = Path(str(models_path) + "/2022-12-01 02 08 35.370858-TrafficIntersection-{}LaneGUI-ppo-1925000".format(TRAFFIC_INTERSECTION_TYPE.capitalize())).resolve()
 model = PPO.load(str(model_path))
 
-def run():
+def run(use_rl: bool = False, connection_label = None):
+
+    time_spent_on_inference = 0
+
+    # choosing the given connection for traffic light switching
+    if connection_label is not None:
+        traci.switch(connection_label)
+
     step = 0
-    if not RL_ON:  # if rl is turned off the continue without intervention
+    if not use_rl:  # if rl is turned off the continue without intervention
         print("\n Reinforcement Learning is Off. \n")
         while step < TOTAL_TIMESTEPS:
             traci.simulationStep()
@@ -140,15 +150,18 @@ def run():
 
                 lanes_observation = [vehicle_count_lane_0, vehicle_count_lane_1, vehicle_count_lane_2, vehicle_count_lane_3]
 
+                before_prediction = time.time()
                 next_configuration, _state = model.predict(lanes_observation, deterministic=True)
+                after_prediction = time.time()
+                time_spent_on_inference += after_prediction - before_prediction
 
                 if next_configuration == current_state / 2: # dividing by 2 as green phases is 0, 2, 4, 6
-                    traci.trafficlight.setPhase(junction_with_lights, current_state)
+                    traci.trafficlight.setPhase(junction_with_lights, int(current_state))
                 else:
                     # Trying to turn on yellow light
                     delta_yellow_time = 0
+                    traci.trafficlight.setPhase(junction_with_lights, int(current_state + 1))
                     while delta_yellow_time < YELLOW_TIME:
-                        traci.trafficlight.setPhase(junction_with_lights, current_state + 1)
                         traci.simulationStep()
                         delta_yellow_time += 1
 
@@ -169,15 +182,18 @@ def run():
                 vehicle_count_lane_7 = traci.lane.getLastStepVehicleNumber("-E11_1")
 
                 lanes_observation = [vehicle_count_lane_0, vehicle_count_lane_1, vehicle_count_lane_2, vehicle_count_lane_3, vehicle_count_lane_4, vehicle_count_lane_5, vehicle_count_lane_6, vehicle_count_lane_7]
+                before_prediction = time.time()
                 next_configuration, _state = model.predict(lanes_observation, deterministic=True)
+                after_prediction = time.time()
+                time_spent_on_inference += after_prediction - before_prediction
 
                 if next_configuration == current_state / 2: # dividing by 2 as green phases is 0, 2, 4, 6
                     traci.trafficlight.setPhase(junction_with_lights, current_state)
                 else:
                     # Trying to turn on yellow light
                     delta_yellow_time = 0
+                    traci.trafficlight.setPhase(junction_with_lights, current_state + 1)
                     while delta_yellow_time < YELLOW_TIME:
-                        traci.trafficlight.setPhase(junction_with_lights, current_state + 1)
                         traci.simulationStep()
                         delta_yellow_time += 1
 
@@ -196,26 +212,101 @@ def run():
                 for i, lane in enumerate(lanes_to_observe):
                     lanes_observation[i] = traci.lane.getLastStepVehicleNumber(lane)
                     
+                before_prediction = time.time()
                 next_configuration, _state = model.predict(lanes_observation, deterministic=True)
+                after_prediction = time.time()
+                time_spent_on_inference += after_prediction - before_prediction
 
                 if next_configuration == current_state / 2: # dividing by 2 as green phases is 0, 2, 4, 6
                     traci.trafficlight.setPhase(junction_with_lights, current_state)
                 else:
                     # Trying to turn on yellow light
                     delta_yellow_time = 0
+                    traci.trafficlight.setPhase(junction_with_lights, current_state + 1)
                     while delta_yellow_time < YELLOW_TIME:
-                        traci.trafficlight.setPhase(junction_with_lights, current_state + 1)
                         traci.simulationStep()
                         delta_yellow_time += 1
+                
 
             # Turning green light for the predefined period of time
             delta_green_time = 0
+            traci.trafficlight.setPhase(junction_with_lights, int(next_configuration * 2))
             while delta_green_time < MIN_GREEN_TIME:
-                traci.trafficlight.setPhase(junction_with_lights, next_configuration * 2)
                 traci.simulationStep()
                 delta_green_time += 1
 
             step = traci.simulation.getTime()
+        
+        if step >= TOTAL_TIMESTEPS:
+            print(f"Total time spend in inference by model: {time_spent_on_inference}")
+
+
+def start_logging(use_rl: bool, log_to_file: bool, nogui: bool = True, unique_identifier: str | int =  time.time()) -> Path | None:
+    global net_file, route_file, sumocfg_file, additional_file_rl, additional_file_no_rl, logging_output_folder
+
+    if log_to_file:
+        output_file = Path(str(logging_output_folder) + \
+        f"/log-output-{'rl' if use_rl else 'no-rl'}-{unique_identifier}.xml").resolve().__str__()
+
+    # this script has been called from the command line. It will start sumo as a
+    # server, then connect and run
+    if nogui:
+        sumoBinary = sumolib.checkBinary('sumo')
+    else:
+        sumoBinary = sumolib.checkBinary('sumo-gui')
+
+    # Generating custom route file
+    if GENERATE_CUSTOM_ROUTE and TRAFFIC_INTERSECTION_TYPE == "double":
+        route_file = generate_routefile()
+
+    # Generating a additional file for logging
+    if log_to_file:
+        if not os.path.exists(logging_output_folder):
+            os.mkdir(logging_output_folder)
+                
+        setupLaneCounting(begin = 0, end = TOTAL_TIMESTEPS,
+        trafficLightSwitchingTime=MIN_GREEN_TIME,
+        yellowLightTime=YELLOW_TIME,
+        outputFile= output_file,
+        additionalFileGenerationPath=additional_file_rl if use_rl else additional_file_no_rl)
+    
+    connection_label = f"rl-{unique_identifier}" if use_rl else f"norl-{unique_identifier}"
+    
+    # this is the normal way of using traci. sumo is started as a
+    # subprocess and then the python script connects and runs
+    if log_to_file:
+        traci.start([sumoBinary,
+        "-n", str(net_file),
+        "-r", str(route_file),
+        "--time-to-teleport", time_to_teleport,
+        "-a", str(additional_file_rl if use_rl else additional_file_no_rl)], label=connection_label)
+    else:
+        traci.start([sumoBinary,
+        "-n", str(net_file),
+        "-r", str(route_file),
+        "--time-to-teleport", time_to_teleport], label=connection_label)
+    
+       
+
+    run(use_rl=use_rl,connection_label=connection_label)
+
+    traci.switch(connection_label)
+    traci.close()
+
+    if log_to_file:
+        convert_to_csv(output_file)
+    
+    return output_file if log_to_file else None
+
+
+
+def convert_to_csv(filename: str):
+    global LOG_TO_FILE, CONVERT_LOG_TO_CSV
+    # this way is a bit hacky, but solves import error because of different modules
+    if LOG_TO_FILE and CONVERT_LOG_TO_CSV:
+        subprocess.call([Path(str(tools) + "/xml/xml2csv.py").resolve().__str__(), str(filename)])
+        print(f"\nOutput file {filename} to csv successfully.")
+
 
 def get_options():
     optParser = optparse.OptionParser()
@@ -232,82 +323,70 @@ def get_options():
     options, args = optParser.parse_args()
     return options
 
+def start(nogui=True, logging_off=False, convert_to_csv=True, turn_off_rl=False, generate_both_outputs=True):
+
+    global LOG_TO_FILE, CONVERT_LOG_TO_CSV, RL_ON, BOTH_OUTPUTS
+
+    if logging_off:
+        LOG_TO_FILE = False
+    if nogui:
+        CONVERT_LOG_TO_CSV = True
+    if turn_off_rl:
+        RL_ON = False
+    if generate_both_outputs:
+        BOTH_OUTPUTS = True
+
+    common_time = time.time()
+
+    no_rl_kwargs = {
+                "use_rl": False,
+                "log_to_file": True,
+                "nogui": nogui,
+                "unique_identifier": common_time,
+            }
+
+    rl_kwargs = no_rl_kwargs.copy()
+    rl_kwargs["use_rl"] = True
+
+    if not os.path.exists(logging_output_folder):
+        os.mkdir(logging_output_folder)
+
+
+    if BOTH_OUTPUTS:
+        
+        rl_process = Process(group=None, target=start_logging, kwargs=rl_kwargs)
+        rl_process.start()
+        no_rl_process = Process(group=None, target=start_logging, kwargs=no_rl_kwargs)
+        no_rl_process.start()
+
+        if not rl_process.is_alive():
+            print(f"RL logging has stopped, exit code {rl_process.exitcode}")
+        
+        if not no_rl_process.is_alive():
+            print(f"No RL logging has stopped, exit code {no_rl_process.exitcode}")
+
+        rl_process.join()
+        no_rl_process.join() 
+
+    elif not turn_off_rl:
+        output_file = start_logging(**rl_kwargs)
+
+        if output_file is not None:
+            print(f"Generation of {output_file} succeded!")
+        else:
+            print(f"Generation of CSV failed!")
+    else:
+        output_file = start_logging(**no_rl_kwargs)
+
+        if output_file is not None:
+            print(f"Generation of {output_file} succeded!")
+        else:
+            print(f"Generation of CSV failed!")
+
+
+
 if __name__ == "__main__":
+
     options = get_options()
 
-    if options.logging_off:
-        LOG_TO_FILE = False
-    if options.convert_to_csv:
-        CONVERT_LOG_TO_CSV = True
-    if options.turn_off_rl:
-        RL_ON = False
-    if options.generate_both_outputs:
-        BOTH_OUTPUTS = True
-        loops = 2
-    else:
-        loops = 1
-
-    if LOG_TO_FILE:
-        if not os.path.exists(logging_output_folder):
-            os.mkdir(logging_output_folder)
-
-        common_time = time.time()
-
-    for i in range(loops):
-        if i == 1:  # turning off rl in the second loop
-            RL_ON = False
-        
-        if LOG_TO_FILE:
-            outputFile = Path(str(logging_output_folder) + \
-            f"/log-output-{'rl' if RL_ON else 'no-rl'}-{common_time}.xml").resolve().__str__()
-
-        # this script has been called from the command line. It will start sumo as a
-        # server, then connect and run
-        if options.nogui:
-            sumoBinary = sumolib.checkBinary('sumo')
-        else:
-            sumoBinary = sumolib.checkBinary('sumo-gui')
-
-        # Generating custom route file
-        if GENERATE_CUSTOM_ROUTE and TRAFFIC_INTERSECTION_TYPE == "double":
-            route_file = generate_routefile()
-
-        # Generating a additional file for logging
-        if LOG_TO_FILE:
-            if not os.path.exists(logging_output_folder):
-                os.mkdir(logging_output_folder)
-
-            if BOTH_OUTPUTS:
-                csv_id = str(i)
-            else:
-                csv_id = ""
-
-            setupLaneCounting(begin = 0, end = TOTAL_TIMESTEPS,
-            trafficLightSwitchingTime=MIN_GREEN_TIME,
-            yellowLightTime=YELLOW_TIME,
-            outputFile= outputFile,
-            additionalFileGenerationPath=additional_file)
-    
-        # this is the normal way of using traci. sumo is started as a
-        # subprocess and then the python script connects and runs
-        if LOG_TO_FILE:
-            traci.start([sumoBinary,
-            "-n", net_file,
-            "-r", route_file,
-            "--time-to-teleport", time_to_teleport,
-            "-a", additional_file])
-        else:
-            traci.start([sumoBinary,
-            "-n", net_file,
-            "-r", route_file,
-            "--time-to-teleport", time_to_teleport])        
-
-        run()
-
-        traci.close()
-
-        # this way is a bit hacky, but solves import error because of different modules
-        if LOG_TO_FILE and CONVERT_LOG_TO_CSV:
-            subprocess.call([Path(str(tools) + "/xml/xml2csv.py").resolve().__str__(), str(outputFile)])
-            print(f"\nOutput file {outputFile} to csv successfully.")
-        
+    start(**vars(options))
